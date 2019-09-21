e3063b448bc36f2a16171e2552336d5e3f134880
bjar <santyyvii@gmail.com>
2019-09-20 23:28:31 -0300

    Se implemento un login inicial y logout

--- app_tipojustificacion/views.py
+++ app_tipojustificacion/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
-
+@login_required
 def index(request):
     tiposjust = TipoJustificacion.objects.all()
     return render(request, 'app_tipojustificacion/index.html', {'tiposjust':tiposjust})
