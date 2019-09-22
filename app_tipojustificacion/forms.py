73e9a52136a48f155d140db1eca91ee5528ca4d3
bjar <santyyvii@gmail.com>
2019-09-22 02:53:28 -0300

    Se usaron widgets en los forms de app_tipojustificacion

--- app_tipojustificacion/forms.py
+++ app_tipojustificacion/forms.py
@@ -5,3 +5,12 @@ class TipoJustificacionForm(forms.ModelForm):
     class Meta:
         model = TipoJustificacion
         fields = ['motivo', 'cargo', 'artcct', 'dia_trabajado','cant_mes', 'cant_año', 'claustro']
+        widgets = {
+            'motivo' : forms.TextInput(attrs={'class' : 'form-control col-md-7 col-xs-12'}),
+            'cargo' : forms.Select(attrs={'class' : 'form-control col-md-7 col-xs-12'}),
+            'artcct' : forms.TextInput(attrs={'class' : 'form-control col-md-7 col-xs-12'}),
+            'dia_trabajado' : forms.CheckboxInput(attrs={'class' : 'flat'}),
+            'cant_mes' : forms.NumberInput(attrs={'class' : 'form-control col-md-7 col-xs-12'}),          
+            'cant_año' : forms.NumberInput(attrs={'class' : 'form-control col-md-7 col-xs-12'}),
+            'claustro' : forms.Select(attrs={'class' : 'form-control col-md-7 col-xs-12'}),          
+        }
