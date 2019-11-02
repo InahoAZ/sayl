b3bad8f94aa4d740848ed8c43dd486a5db146bfd
bjar <santyyvii@gmail.com>
2019-11-02 16:38:53 -0300

    se instalo una libreria para manejar tasks, falta implementar.

---
+++ asistencias/tasks.py
@@ -0,0 +1,8 @@
+from background_task import background
+from django.contrib.auth.models import User
+
+@background(schedule=60)
+def notify_user(user_id):
+    # lookup user by id and send them a message
+    user = User.objects.get(pk=user_id)
+    user.email_user('Here is a notification', 'You have been notified')
